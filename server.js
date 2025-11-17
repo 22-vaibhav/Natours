fb12e9766911e49edd230bba54b9e49f7e83850b
Vaibhav Prakash <vaibhavprakash180@gmail.com>
2025-11-17 17:16:31 +0530

    fix: disable dotenv on production

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: `${__dirname}/config.env` });
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: './config.env' });
+}
 
 const Tour = require(`${__dirname}/models/tourModel`)
 
