daf4d336d3745ced91af619accd8eb5d8e46b4f8
Vaibhav Prakash <vaibhavprakash180@gmail.com>
2025-11-17 17:08:08 +0530

    Error handling

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: `${__dirname}/config.env`});
+dotenv.config({ path: `${__dirname}/config.env` });
 
 const Tour = require(`${__dirname}/models/tourModel`)
+
+// Uncaught Exception:
+process.on('uncaughtException', err => {
+    console.log("Uncaught Exception. Shutting Down...");
+    console.log(err.name, err.message);
+    process.exit(1);
+})
 
 const DB = process.env.DATABASE
 
@@ -13,13 +20,16 @@ mongoose.connect(DB, {
 }).then(con => {
     // console.log(con.connections);
     console.log("DB connection successful");
-})
+}).catch(err => {
+    console.error('DB connection error:', err.message);
+    process.exit(1);
+});
 
 const app = require(`${__dirname}/app`);
 
 const port = process.env.PORT || 8000;
 // creating a server using express
-const server = app.listen(port, function(){
+const server = app.listen(port, function () {
     console.log(`App running on port ${port}...`);
 })
 
@@ -27,14 +37,7 @@ const server = app.listen(port, function(){
 process.on('unhandledRejection', err => {
     console.log("Unhandled Rejection. Shutting Down...");
     console.log(err.name, err.message);
-    server.close(()=>{
+    server.close(() => {
         process.exit(1);
     })
-})
-
-// Uncaught Exception:
-process.on('uncaughtException', err => {
-    console.log("Uncaught Exception. Shutting Down...");
-    console.log(err.name, err.message);
-    process.exit(1);
 })
